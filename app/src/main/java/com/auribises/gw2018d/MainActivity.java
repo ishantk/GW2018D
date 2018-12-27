70cf7c34093308cd3bf023242b2c98c03e511581
Ishant Kumar <er.ishant@gmail.com>
2018-12-27 13:30:04 +0530

    Session3 - Android Activity Life Cycle

--- app/src/main/java/com/auribises/gw2018d/MainActivity.java
+++ app/src/main/java/com/auribises/gw2018d/MainActivity.java
@@ -1,7 +1,9 @@
 package com.auribises.gw2018d;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView txtTitle;
     EditText eTxtNum1, eTxtNum2;
     Button btnAddNumbers;
+
+    final String TAG = "MainActivity";
 
     // User Defined Function/Method
     // Create Objects and Initialize Reference Variables
@@ -38,7 +42,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setContentView(R.layout.activity_main);
 
+        Log.i(TAG,"==onCreate==");
+
         initViews();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"==onStart==");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"==onRestart==");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"==onResume==");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"==onPause==");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"==onStop==");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"==onDestroy==");
     }
 
     @Override
@@ -120,6 +162,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.bol:
                 Toast.makeText(this,"Bollywood Songs Selected",Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+
                 break;
         }
 
