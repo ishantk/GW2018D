bb9e9a7ea3939938bc8ca22c13f127939bfe5ada
Ishant Kumar <er.ishant@gmail.com>
2019-01-03 15:36:29 +0530

    Session7 - Android Fragments

---
+++ app/src/main/java/com/auribises/gw2018d/FragmentDemoActivity.java
@@ -0,0 +1,28 @@
+package com.auribises.gw2018d;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentDemoActivity extends AppCompatActivity {
+
+    UpperFragment upperFragment;
+    LowerFragment lowerFragment;
+    FragmentManager fragmentManager;
+
+    void initViews(){
+        upperFragment = new UpperFragment();
+        lowerFragment = new LowerFragment();
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.upperFrame,upperFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.lowerFrame,lowerFragment).commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_demo);
+        initViews();
+    }
+}
---
+++ app/src/main/java/com/auribises/gw2018d/UpperFragment.java
@@ -0,0 +1,43 @@
+package com.auribises.gw2018d;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UpperFragment extends Fragment implements View.OnClickListener{
+
+
+    Button btnSubmit;
+
+    public UpperFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_upper, container, false);
+        View view = inflater.inflate(R.layout.fragment_upper, container, false);
+
+        btnSubmit = view.findViewById(R.id.buttonSubmit);
+        btnSubmit.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getContext(),"You Clicked button in Upper Fragment",Toast.LENGTH_LONG).show();
+    }
+}
